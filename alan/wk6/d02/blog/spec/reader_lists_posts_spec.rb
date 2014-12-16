5c8f20d610bd27ec3f0e2e8169fb6f37dc6bd2e8
Alan <alanng95@hotmail.com>
2014-12-16 15:20:42 +1100

    reader view thingy

---
+++ alan/wk6/d02/blog/spec/reader_lists_posts_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+feature "ReaderListsPosts", :type => :feature do
+  scenario "there is no blog posts" do
+    visit '/'
+    expect(page).to have_content("No blog posts found")
+  end
+
+  scenario "one blog post" do 
+    Post.create(
+      title: "Hello World",
+      author: "Jane Birken",
+      content: "Lorem Ipsum dolar sit amet"
+      )
+    visit '/'
+    expect(page).to have_content("Hello World")
+    expect(page).to have_content("Jane Birken")
+    expect(page).to have_content("Lorem Ipsum dolar sit amet")
+    expect(page).not_to have_content("No blog posts found")
+  end
+
+  scenario "multiple blog post in system" do
+    (0..3).each do |n|
+      Post.create(
+        title: "Hello World #{n}",
+        author: "Jane Doe #{n}",
+        content: "Lorem Ipsum #{n} dolar sit amet"
+      )
+    end
+
+    visit '/'
+
+  (0..3).each do |n|
+    expect(page).to have_content("Hello World #{n}")
+    expect(page).to have_content("Jane Doe #{n}")
+    expect(page).to have_content("Lorem Ipsum #{n} dolar sit amet")
+    end
+
+    expect(page).not_to have_content("No blog posts found")
+  end
+end
