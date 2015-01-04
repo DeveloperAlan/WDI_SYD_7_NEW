761930cc7b165794b05c78564959b0879ad5752e
Alan <alanng95@hotmail.com>
2015-01-05 10:11:36 +1100

    forgot to commit these stuff

---
+++ alan/bonus/faux-pubsub.js
@@ -0,0 +1,22 @@
+var clientOne = new Client();
+var clientTwo = new Client();
+var clientThree = new Client();
+var clientFour = new Client();
+
+clientOne.connect(server);
+clientTwo.connect(server);
+clientThree.connect(server);
+clientFour.connect(server);
+
+clientOne.subscribe("A", function(message) {
+  console.log(message.text);
+});
+clientOne.subscribe("B");
+clientTwo.subscribe("A");
+clientThree.subscribe("B");
+clientThree.subscribe("C");
+clientFour.subscribe("C");
+
+server.publish("A", {
+  text: "Hello World!";
+})
