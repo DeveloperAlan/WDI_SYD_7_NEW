5ced8c8cd3dcc753850ed6e1011959eef441ac21
Alan <alanng95@hotmail.com>
2015-01-19 10:13:54 +1100

    added some seed json data

--- alan/wk09/d01/twitter_clone/db/seeds.rb
+++ alan/wk09/d01/twitter_clone/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Creating some seed statuses"
+
+Status.create({
+  username: "FluffyJack",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis mollitia vitae pariatur, voluptate inventore ea iusto quisquam sunt nemo odit soluta temporibus deleniti, hic architecto in ipsam dicta! Quibusdam, saepe!"
+  })
+
+Status.create({
+  username: "HomemadeIcing",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis mollitia vitae pariatur, voluptate inventore ea iusto quisquam sunt nemo odit soluta temporibus deleniti, hic architecto in ipsam dicta! Quibusdam, saepe!"
+  })
+
+Status.create({
+  username: "JimboJaggins",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis mollitia vitae pariatur, voluptate inventore ea iusto quisquam sunt nemo odit soluta temporibus deleniti, hic architecto in ipsam dicta! Quibusdam, saepe!"
+  })
