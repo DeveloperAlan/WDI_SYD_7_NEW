457ff00e9dda459db19dddbe3080c544832a0f77
Alan <alanng95@hotmail.com>
2015-01-19 13:05:39 +1100

    All the CRUD actions, the javascript files Status and Statuses as well as the seeds. finished product

--- alan/wk09/d01/twitter_clone/app/controllers/statuses_controller.rb
+++ alan/wk09/d01/twitter_clone/app/controllers/statuses_controller.rb
@@ -6,4 +6,35 @@ class StatusesController < ApplicationController
       f.json { render json: @statuses}
     end
   end
+
+  def create 
+    @create = Status.create(status_params)
+
+    respond_to do |f|
+      f.json { render json: @status}
+    end
+  end
+
+  def update
+    @status = Status.find(params[:id])
+    @status.update(status_params)
+
+    respond_to do |f|
+      f.json { render json: @status}
+    end
+  end
+
+  def destroy 
+    @status = Status.find(params[:id])
+    @status.destroy()
+
+    respond_to do |f|
+      f.json { render.json {status: "ok"} }
+    end
+
+  private
+
+  def status_params
+    params.require(:status).permit(:username, :content)
+  end
 end
