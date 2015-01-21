4c891732ea4cff537dc4f3d8799df32cd64b42e6
Alan <alanng95@hotmail.com>
2015-01-21 11:38:42 +1100

    finished rendering on timelineview and doing statusview

--- alan/wk09/d01/twitter_clone/app/assets/javascripts/views/TimelineView.js
+++ alan/wk09/d01/twitter_clone/app/assets/javascripts/views/TimelineView.js
@@ -2,9 +2,26 @@ var TimelineView = Backbone.View.extend({
   tagName: "div",
   className: "timeline",
 
-  template: JST['statuses/timeline'], 
+  // template: JST['statuses/timeline'], 
 
+  initialize: function() {
+    this.collection.fetch();
+    this.collection.on("sync", this.render, this);
+  },
+
+  // render: function() {
+  //   this.$el.html(
+  //     this.template({
+  //       statuses: this.collection.toJSON()
+  //     })
+  //   );
+  // }
+
+  
   render: function() {
-    
+    this.collection.each(function (status) {
+      var statusView = new StatusView({ model: status});
+    });
   }
+  
 });
